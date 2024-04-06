88c94bf2a89f06b679c4dc181fc29aeeae0a50f4
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-04-06 10:33:39 +0530

    Update __init__.py

--- X/helpers/SQL/__init__.py
+++ X/helpers/SQL/__init__.py
@@ -1,39 +1,43 @@
 import os
-from sqlalchemy import create_engine, exc
+import motor.motor_asyncio
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-
 from X import LOGGER
 
-BASE = declarative_base()
+DB_URL = "mongodb+srv://public:abishnoimf@cluster0.rqk6ihd.mongodb.net/?retryWrites=true&w=majority"
+SPAMBOT = "SPAMBOT"
 
-def start(db_url) -> scoped_session:
-    try:
-        engine = create_engine(db_url)
-        BASE.metadata.bind = engine
-        BASE.metadata.create_all(engine)
-        return scoped_session(sessionmaker(bind=engine, autoflush=False))
-    except exc.OperationalError as e:
-        LOGGER(__name__).warning(
-            "Failed to connect to the database. Check your DB_URL configuration."
-        )
-        LOGGER(__name__).info(str(e))
-        return None
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URL)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    LOGGER(__name__).warning(
+        "DB_URI is not configured. Features depending on the database might have issues."
+    )
+    LOGGER(__name__).info(str(e))
 
 
 DB_AVAILABLE = False
+BOTINLINE_AVAIABLE = False
 
-def mulaisql(db_url) -> scoped_session:
+
+def mulaisql() -> scoped_session:
     global DB_AVAILABLE
+    engine = create_engine(DB_URL, client_encoding="utf8")
+    BASE.metadata.bind = engine
     try:
-        engine = create_engine(db_url, client_encoding="utf8")
-        BASE.metadata.bind = engine
         BASE.metadata.create_all(engine)
-        DB_AVAILABLE = True
-        return scoped_session(sessionmaker(bind=engine, autoflush=False))
     except exc.OperationalError:
         DB_AVAILABLE = False
-        LOGGER(__name__).warning(
-            "Failed to connect to the database. Check your DB_URL configuration."
-        )
-        return None
+        return False
+    DB_AVAILABLE = True
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
