9f7d0b8aff3670a2befa8fc0364a347a0281e1cb
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-04-08 22:06:27 +0530

    Update eval.py

--- X/modules/user/eval.py
+++ X/modules/user/eval.py
@@ -2,33 +2,39 @@ import sys
 from pyrogram import Client, filters
 from io import StringIO
 from config import OWNER_ID
+import traceback
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command(["eval"], ["."]))
 def user_exec(client, message):
-    reply = message.reply_to_message
-    code = ""
-    try:
-        code = message.text.split(" ", maxsplit=1)[1]
-    except IndexError:
+    if message.reply_to_message:
+        code = message.reply_to_message.text.html if message.reply_to_message.text else ""
+    else:
         try:
-            code = message.text.split(" \n", maxsplit=1)[1]
+            code = message.text.html.split(" ", maxsplit=1)[1]
         except IndexError:
-            pass
+            code = ""
 
-    result = sys.stdout = StringIO()
+    if not code:
+        message.edit("No code provided.")
+        return
+
+    result = StringIO()
+    sys.stdout = result
     try:
-        exec(code)
-
+        exec(code, globals(), locals(), timeout=10)  # Timeout set to 10 seconds
+        output = result.getvalue()
         message.edit(
             f"<b>Code:</b>\n"
             f"<code>{code}</code>\n\n"
             f"<b>Result</b>:\n"
-            f"<code>{result.getvalue()}</code>"
+            f"<code>{output}</code>"
         )
-    except:
+    except Exception as e:
         message.edit(
             f"<b>Code:</b>\n"
-            f"<code>```python{code}```</code>\n\n"
-            f"<b>Result</b>:\n"
-            f"<code>{sys.exc_info()[0].__name__}: {sys.exc_info()[1]}</code>"
+            f"<code>{code}</code>\n\n"
+            f"<b>Error:</b>\n"
+            f"<code>{str(e)}</code>"
         )
+    finally:
+        sys.stdout = sys.__stdout__
