68f454f34d4a7bc202b2fc0bc34eeaa9084bc652
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-10 00:07:14 +0530

    Update hang.py

--- X/modules/user/hang.py
+++ X/modules/user/hang.py
@@ -1,1 +1,8 @@
+import asyncio
+from random import choice
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from config import OWNER_ID
+from config import CMD_HANDLER as cmd
+from .help import *
 
