24cfc1c98b933a46dc21a36cf3c52bb0415178e9
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-04-09 10:55:50 +0530

    Update abuse.py

--- X/modules/user/abuse.py
+++ X/modules/user/abuse.py
@@ -1,31 +1,31 @@
 from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from STORMDB.data import STORMS, ABUSE
+from XDB.data import MASTERS, ABUSE
 from config import SUDO_USERS, OWNER_ID
 import asyncio
 
 @Client.on_message(filters.user(SUDO_USERS) & filters.command(["abuse", "teri"], ["."]))
-async def abuse(xspam: Client, e: Message):
-    kex = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+async def abuse(x: Client, e: Message):
+    NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
 
-    if len(kex) == 2:
-        ok = await xspam.get_users(kex[1])
-        counts = int(kex[0])
+    if len(NOBI) == 2:
+        ok = await x.get_users(NOBI[1])
+        counts = int(NOBI[0])
         for _ in range(counts):
             reply = choice(ABUSE)
             msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
-            await xspam.send_message(e.chat.id, msg)
+            await x.send_message(e.chat.id, msg)
             await asyncio.sleep(0.1)
 
     elif e.reply_to_message:
         user_id = e.reply_to_message.from_user.id
-        ok = await xspam.get_users(user_id)
-        counts = int(kex[0])
+        ok = await x.get_users(user_id)
+        counts = int(NOBI[0])
         for _ in range(counts):
             reply = choice(ABUSE)
             msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
-            await xspam.send_message(e.chat.id, msg)
+            await x.send_message(e.chat.id, msg)
             await asyncio.sleep(0.1)
 
     else:
