fa437387cdda3cec96920ad6b0b766945536476e
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-14 11:13:05 +0530

    Update corona.py

--- X/modules/user/corona.py
+++ X/modules/user/corona.py
@@ -14,7 +14,7 @@ from .help import *
 @Client.on_message(filters.command("corona", cmd) & filters.me)
 async def corona_all(client, message):
     try:
-        r = await AioHttp().get_json("https://api-ninjas.com/api/covid19/all?yesterday=true")
+        r = await AioHttp().get_json("https://api-ninjas.com/api/covid19")
         last_updated = datetime.datetime.fromtimestamp(r["updated"] / 1000).strftime(
             "%Y-%m-%d %I:%M:%S"
         )
