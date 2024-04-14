ef4e4b11beb84c8d62dfee63648ce2c78d87656c
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-14 14:24:57 +0530

    Update g_drive.py

--- X/modules/user/g_drive.py
+++ X/modules/user/g_drive.py
@@ -5,6 +5,8 @@ from pyrogram.types import (
     Message,
 )
 from pyrogram.enums.parse_mode import ParseMode  
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 from pyrogram import Client, filters
 from config import OWNER_ID
 from config import CMD_HANDLER as cmd
