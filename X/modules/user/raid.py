d1dfb30aee267f64aca1767802049eeb93bcc5d9
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-16 20:38:00 +0530

    Update raid.py added Rᴀɪᴅ

--- X/modules/user/raid.py
+++ X/modules/user/raid.py
@@ -1,1 +1,42 @@
-#coming soon  
+import asyncio
+from random import choice
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from config import OWNER_ID
+from config import CMD_HANDLER as cmd
+from XDB.data import RAID
+from .help import *
+
+@Client.on_message(filters.command("raid", cmd) & filters.me)
+async def raid(x: Client, e: Message):
+      NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+
+      if len(NOBI) == 2:
+          ok = await x.get_users(kex[1])
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(RAID)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      elif e.reply_to_message:
+          user_id = e.reply_to_message.from_user.id
+          ok = await x.get_users(user_id)
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(RAID)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      else:
+            await e.reply_text(".Rᴀɪᴅ 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")  
+
+
+add_command_help(
+    "•─╼⃝𖠁 Rᴀɪᴅ",
+    [
+        ["raid", "Tᴏ ꜱᴇɴᴅ Rᴀɪᴅ Mᴇssᴀɢᴇs."],
+    ],
+)
