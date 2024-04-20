41b3a4fc3255154d3a3f101773ffc898fdd71036
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-20 21:46:00 +0530

    Update radhaji.py

--- X/modules/user/radhaji.py
+++ X/modules/user/radhaji.py
@@ -1,1 +1,42 @@
-#coming soon 
+import asyncio
+from random import choice
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from config import OWNER_ID
+from config import CMD_HANDLER as cmd
+from SANATANDB.sanatan import RADHAJI
+from .help import *
+
+@Client.on_message(filters.command("radhaji", cmd) & filters.me)
+async def radhaji(x: Client, e: Message):
+      NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+
+      if len(NOBI) == 2:
+          ok = await x.get_users(NOBI[1])
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(RADHAJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      elif e.reply_to_message:
+          user_id = e.reply_to_message.from_user.id
+          ok = await x.get_users(user_id)
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(RADHAJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      else:
+            await e.reply_text(". radhaji 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")  
+
+
+add_command_help(
+    "•─╼⃝𖠁 Rᴀᴅʜᴀ ᴊɪ",
+    [
+        ["radhaji", "Tᴏ ꜱᴇɴᴅ Rᴀᴅʜᴀ ᴊɪ Mᴇssᴀɢᴇs."],
+    ],
+)
