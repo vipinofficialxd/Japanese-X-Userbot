e03d1f79ce59bdf3afaf14ae5a784c9092ca0793
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-20 23:00:45 +0530

    Update ANIME.py

--- ANIMEDB/ANIME.py
+++ ANIMEDB/ANIME.py
@@ -1,4 +1,10 @@
 MASTERS = [6694740726]
 
 NARUTO = [
+  "https://telegra.ph/file/d795474451011853d3cf8.jpg",
+  "https://telegra.ph/file/74a8a5b45d39582aecaf8.jpg",
+  "https://telegra.ph/file/62647a608e48973a3386a.jpg",
+  "https://telegra.ph/file/44c24d68d7707e88f519a.jpg",
+  "https://telegra.ph/file/d74f7802a18c5c93e6a1b.jpg",
+]
 
