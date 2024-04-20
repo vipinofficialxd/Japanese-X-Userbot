d95d700a9cca5074893629549fac60e2304f9357
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-20 23:02:58 +0530

    Update ANIME.py

--- ANIMEDB/ANIME.py
+++ ANIMEDB/ANIME.py
@@ -6,5 +6,8 @@ NARUTO = [
   "https://telegra.ph/file/62647a608e48973a3386a.jpg",
   "https://telegra.ph/file/44c24d68d7707e88f519a.jpg",
   "https://telegra.ph/file/d74f7802a18c5c93e6a1b.jpg",
+  "https://telegra.ph/file/268b06eba93ea96b4792f.jpg",
+  "https://telegra.ph/file/a939610204fdf7a70c871.jpg",
+  "https://telegra.ph/file/ebf66fb6740cc1dbe589b.jpg",
 ]
 
