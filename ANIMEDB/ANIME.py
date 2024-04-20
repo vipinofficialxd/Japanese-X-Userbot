5ce214d5b58bac6e9950ecb62e5731364e9f5a39
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-20 23:05:11 +0530

    Update ANIME.py

--- ANIMEDB/ANIME.py
+++ ANIMEDB/ANIME.py
@@ -11,3 +11,10 @@ NARUTO = [
   "https://telegra.ph/file/ebf66fb6740cc1dbe589b.jpg",
 ]
 
+NEZUKO = [
+  "https://telegra.ph/file/950c093cb3ce67555ce36.jpg",
+  "https://telegra.ph/file/5b2c08f0b85cbbfed74fd.jpg",
+  "https://telegra.ph/file/a772942699d78040a3dcf.jpg",
+  "https://telegra.ph/file/c13d1de9fa933c8fed7b7.jpg",
+  "https://telegra.ph/file/6f41d905efae9ae332ec4.jpg",
+]
