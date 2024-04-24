a311fb1c7769f074786c58232bea8b2c2d79b7d0
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-24 11:42:35 +0530

    Update mentiontag.py

--- X/modules/user/mentiontag.py
+++ X/modules/user/mentiontag.py
@@ -1,3 +1,14 @@
+# Copyright (C) 2020-2021 by Toni880@Github, < https://github.com/Toni880 >.
+#
+# This file is part of < https://github.com/Toni880/Prime-Userbot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/Toni880/Prime-Userbot/blob/master/LICENSE >
+#
+# All rights reserved. Ken-Kan
+
+# REMAKE BY NOBITA XD AND TRYTOLIVEALONE 
+
+
 from asyncio import sleep
 
 from pyrogram import Client, filters
