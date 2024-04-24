17b4fdd84e8b60a39c0a3851e919b7433a25c3ba
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-24 08:57:32 +0530

    Update clone.py

--- X/modules/user/clone.py
+++ X/modules/user/clone.py
@@ -1,3 +1,18 @@
+# Credits: @mrismanaziz
+# Copyright (C) 2022 Pyro-ManUserbot
+#
+# This file is a part of < https://github.com/mrismanaziz/PyroMan-Userbot/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/mrismanaziz/PyroMan-Userbot/blob/main/LICENSE/>.
+#
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+
+#REMAKE BY : NOBITA XD AND TRYTOLIVEALONE
+
+
+
+
 import os
 
 from pyrogram import *
