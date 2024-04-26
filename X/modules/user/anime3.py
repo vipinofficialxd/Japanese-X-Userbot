3a7d38749e10a971bdd912770fe63688e47744dd
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-26 20:51:28 +0530

    Create anime3.py

---
+++ X/modules/user/anime3.py
@@ -0,0 +1,53 @@
+#CREDIT : GURU BOT THANKS FOR YOUR ANIME API 
+
+#REMAKE BY : NOBITA XD AND TRYTOLIVEALONE
+# Copyright (C) 2024 JAPANESE X USERBOT
+#DON'T KANG FUCKING COWARD
+#BSDKE KANG KIYA TOH SOCH LIYO
+#AAG LAGA DUNGA TERE ANDAR 
+#SAMJHA ? 
+
+
+
+
+
+
+
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import CMD_HANDLER as cmd
+
+from .help import * 
+
+API_URL = "https://api.nekosapi.com/v3/images/16"
+
+
+@Client.on_message(filters.command("anime3", cmd) & filters.me)
+async def anime3(client: Client, message: Message):
+    # Send the "Processing..." message
+    await message.edit("Fetching a random anime image...")
+
+    # Make the API request
+    try:
+        response = requests.get(API_URL)
+        response.raise_for_status()
+        data = response.json()["data"]["attributes"]
+        image_url = data["file"]
+        title = data["title"]
+    except (requests.exceptions.RequestException, KeyError):
+        await message.edit("Failed to fetch a random anime image.")
+        return
+
+    # Send the image and title as a reply
+    await client.send_photo(message.chat.id, image_url, caption=f"**Title:** {title}")
+
+    # Edit the original message to indicate success
+    await message.edit("Random anime image sent!")
+
+add_command_help(
+    "•─╼⃝𖠁 ᴀɴɪᴍᴇ3",
+    [
+       ["anime3", "Gɪᴠᴇ random anime pic."],
+        ],
+)
