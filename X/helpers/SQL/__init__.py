b273f4e276435110c13491a10d8a42192ab8521f
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-04-27 13:17:43 +0530

    ᴊᴀᴘᴀɴᴇꜱᴇ x ᴜꜱᴇʀʙᴏᴛ

---
+++ X/helpers/SQL/__init__.py
@@ -0,0 +1,66 @@
+#MIT License
+
+#Copyright (c) 2024 Japanese-X-Userbot
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+import os
+
+import motor.motor_asyncio
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from X import DB_URL, LOGGER
+
+SPAMBOT = "SPAMBOT"
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URL)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+try:
+    BASE = declarative_base()
+    SESSION = start()
+except AttributeError as e:
+    LOGGER(__name__).warning(
+        "DB_URI is not configured. Features depending on the database might have issues."
+    )
+    LOGGER(__name__).info(str(e))
+
+
+DB_AVAILABLE = False
+BOTINLINE_AVAIABLE = False
+
+
+def mulaisql() -> scoped_session:
+    global DB_AVAILABLE
+    engine = create_engine(DB_URL, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    try:
+        BASE.metadata.create_all(engine)
+    except exc.OperationalError:
+        DB_AVAILABLE = False
+        return False
+    DB_AVAILABLE = True
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
