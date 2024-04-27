a2c5c2c5bb3798ffbac0946a035ac32a98b075fb
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-27 23:22:06 +0530

    Update saraswatiji.py Sᴀʀᴀsᴡᴀᴛɪ Ji

--- X/modules/user/saraswatiji.py
+++ X/modules/user/saraswatiji.py
@@ -20,4 +20,54 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#coming soon 
+# Credits: NOBITA XD 
+# Copyright (C) 2024 JAPANESE X USERBOT
+#DON'T KANG FUCKING COWARD
+#BSDKE KANG KIYA TOH SOCH LIYO
+#AAG LAGA DUNGA TERE ANDAR 
+#SAMJHA ? 
+
+
+
+import asyncio
+from random import choice
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from config import OWNER_ID
+from config import CMD_HANDLER as cmd
+from SANATANDB.sanatan import SARASWATIJI
+from .help import *
+
+@Client.on_message(filters.command("saraswatiji", cmd) & filters.me)
+async def saraswatiji(x: Client, e: Message):
+      NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+
+      if len(NOBI) == 2:
+          ok = await x.get_users(NOBI[1])
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(SARASWATIJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      elif e.reply_to_message:
+          user_id = e.reply_to_message.from_user.id
+          ok = await x.get_users(user_id)
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(SARASWATIJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      else:
+            await e.reply_text(".saraswatiji 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")  
+
+
+add_command_help(
+    "•─╼⃝𖠁 Sᴀʀᴀsᴡᴀᴛɪ ᴊɪ",
+    [
+        ["saraswatiji", "Tᴏ ꜱᴇɴᴅ Sᴀʀᴀsᴡᴀᴛɪ ᴊɪ Mᴇssᴀɢᴇs."],
+    ],
+)
