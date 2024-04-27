11605baed021ec8435c6f8f50e7cf82a6310c215
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-27 21:03:11 +0530

    Update riddhiji.py ʀɪᴅᴅʜɪ ᴊɪ

--- X/modules/user/riddhiji.py
+++ X/modules/user/riddhiji.py
@@ -20,4 +20,54 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-#comimg soon 
+#CREDIT : NOBITA XD
+# Copyright (C) 2024 JAPANESE X USERBOT
+#DON'T KANG FUCKING COWARD
+#BSDKE KANG KIYA TOH SOCH LIYO
+#AAG LAGA DUNGA TERE ANDAR 
+#SAMJHA ? 
+
+
+
+import asyncio
+from random import choice
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from config import OWNER_ID
+from config import CMD_HANDLER as cmd
+from SANATANDB.sanatan import RIDDHIJI
+from .help import *
+
+@Client.on_message(filters.command("riddhiji", cmd) & filters.me)
+async def riddhiji(x: Client, e: Message):
+      NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+
+      if len(NOBI) == 2:
+          ok = await x.get_users(NOBI[1])
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(RIDDHIJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      elif e.reply_to_message:
+          user_id = e.reply_to_message.from_user.id
+          ok = await x.get_users(user_id)
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(RIDDHIJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      else:
+            await e.reply_text(". riddhiji 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")  
+
+
+add_command_help(
+    "•─╼⃝𖠁 ʀɪᴅᴅʜɪ ᴊɪ",
+    [
+        ["riddhiji", "Tᴏ ꜱᴇɴᴅ ʀɪᴅᴅʜɪ ᴊɪ Mᴇssᴀɢᴇs."],
+    ],
+)
