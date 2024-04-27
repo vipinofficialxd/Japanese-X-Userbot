8d6ee5bc207bc522c42b829be061c3a15cdc360e
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-04-27 22:34:10 +0530

    Update narayanji.py Nᴀʀᴀʏᴀɴ ᴊɪ

--- X/modules/user/narayanji.py
+++ X/modules/user/narayanji.py
@@ -26,3 +26,46 @@
 #BSDKE KANG KIYA TOH SOCH LIYO
 #AAG LAGA DUNGA TERE ANDAR 
 #SAMJHA ? 
+
+import asyncio
+from random import choice
+from pyrogram.types import Message
+from pyrogram import filters, Client
+from config import OWNER_ID
+from config import CMD_HANDLER as cmd
+from SANATANDB.sanatan import NARAYANJI
+from .help import *
+
+@Client.on_message(filters.command("narayanji", cmd) & filters.me)
+async def narayanji(x: Client, e: Message):
+      NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
+
+      if len(NOBI) == 2:
+          ok = await x.get_users(NOBI[1])
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(NARAYANJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      elif e.reply_to_message:
+          user_id = e.reply_to_message.from_user.id
+          ok = await x.get_users(user_id)
+          counts = int(NOBI[0])
+          for _ in range(counts):
+                reply = choice(NARAYANJI)
+                msg = f"[{ok.first_name}](tg://user?id={ok.id}) {reply}"
+                await x.send_message(e.chat.id, msg)
+                await asyncio.sleep(0.1)
+
+      else:
+            await e.reply_text(". narayanji 10 <ʀᴇᴘʟʏ ᴛᴏ ᴜꜱᴇʀ ᴏʀ ᴜꜱᴇʀɴᴀᴍᴇ>")  
+
+
+add_command_help(
+    "•─╼⃝𖠁 Nᴀʀᴀʏᴀɴ ᴊɪ",
+    [
+        ["narayanji", "Tᴏ ꜱᴇɴᴅ Nᴀʀᴀʏᴀɴ ᴊɪ Mᴇssᴀɢᴇs."],
+    ],
+)
