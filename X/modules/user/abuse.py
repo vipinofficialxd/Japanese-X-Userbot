a2144fc33e28d5789fe0d5d644a20833329d09f2
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-05-02 15:50:08 +0530

    Update abuse.py

--- X/modules/user/abuse.py
+++ X/modules/user/abuse.py
@@ -31,7 +31,7 @@ from random import choice
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from XDB.data import MASTERS, ABUSE
-from config import OWNER_ID
+from config import OWNER_ID, SUDO_USERS
 from config import CMD_HANDLER as cmd
 from .help import *
 import asyncio
