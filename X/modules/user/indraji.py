84f15605c48993d65f916cfa4cd9c9b6050f3655
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-05-03 22:15:47 +0530

    Update indraji.py

--- X/modules/user/indraji.py
+++ X/modules/user/indraji.py
@@ -33,11 +33,14 @@ from random import choice
 from pyrogram.types import Message
 from pyrogram import filters, Client
 from config import OWNER_ID
+from config import SUDO_USERS
 from config import CMD_HANDLER as cmd
 from SANATANDB.sanatan import INDRAJI
 from .help import *
 
-@Client.on_message(filters.command("indraji", cmd) & filters.me)
+@Client.on_message(
+    filters.command(["indraji"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
 async def indraji(x: Client, e: Message):
       NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
 
