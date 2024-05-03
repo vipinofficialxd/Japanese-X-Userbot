944fee25ec305b79436522b90c4094c3cd00685c
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-05-03 23:21:35 +0530

    Update git_commands.py

--- X/modules/user/git_commands.py
+++ X/modules/user/git_commands.py
@@ -33,11 +33,14 @@ import aiohttp
 from pyrogram import filters, Client
 from pyrogram.types import Message
 from config import OWNER_ID
+from config import SUDO_USERS
 from config import CMD_HANDLER as cmd
 
 from .help import *
 
-@Client.on_message(filters.command("gitinfo", cmd) & filters.me)
+@Client.on_message(
+    filters.command(["gitinfo"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
 async def githubuser(client: Client, message: Message):
     if len(message.command) != 2:
         await message.reply_text(f". ɢɪᴛɪɴꜰᴏ [ᴜꜱᴇʀɴᴀᴍᴇ]")
