e06613dedbf7dc4ad087e9eeb6f2c0043cf58c2d
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-05-03 20:17:21 +0530

    Update radhaji.py

--- X/modules/user/radhaji.py
+++ X/modules/user/radhaji.py
@@ -34,11 +34,14 @@ from random import choice
 from pyrogram.types import Message
 from pyrogram import filters, Client
 from config import OWNER_ID
+from config import SUDO_USERS
 from config import CMD_HANDLER as cmd
 from SANATANDB.sanatan import RADHAJI
 from .help import *
 
-@Client.on_message(filters.command("radhaji", cmd) & filters.me)
+@Client.on_message(
+    filters.command(["radhaji"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
 async def radhaji(x: Client, e: Message):
       NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
 
