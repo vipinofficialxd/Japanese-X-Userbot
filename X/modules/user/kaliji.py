38df2d325516efba08c97bd0f6661088f9c79067
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-05-03 21:41:40 +0530

    Update kaliji.py

--- X/modules/user/kaliji.py
+++ X/modules/user/kaliji.py
@@ -33,11 +33,14 @@ from random import choice
 from pyrogram.types import Message
 from pyrogram import filters, Client
 from config import OWNER_ID
+from config import SUDO_USERS
 from config import CMD_HANDLER as cmd
 from SANATANDB.sanatan import KALIJI
 from .help import *
 
-@Client.on_message(filters.command("kaliji", cmd) & filters.me)
+@Client.on_message(
+    filters.command(["kaliji"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
 async def kaliji(x: Client, e: Message):
       NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
 
