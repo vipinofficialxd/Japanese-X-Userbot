56b08cbcaaaf9b8ac66abcc517cf61032315bedf
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-05-03 12:03:43 +0530

    Update vishnuji.py

--- X/modules/user/vishnuji.py
+++ X/modules/user/vishnuji.py
@@ -34,11 +34,14 @@ from random import choice
 from pyrogram.types import Message
 from pyrogram import filters, Client
 from config import OWNER_ID
+from config import SUDO_USERS
 from config import CMD_HANDLER as cmd
 from SANATANDB.sanatan import VISHNUJI
 from .help import *
 
-@Client.on_message(filters.command("vishnuji", cmd) & filters.me)
+@Client.on_message(
+    filters.command(["vishnuji"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
 async def vishnuji(x: Client, e: Message):
       NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
 
