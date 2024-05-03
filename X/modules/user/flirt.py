49d148c821e4594de267f47c92f2476398c58496
𝙽𝙾𝙱𝙸𝚃𝙰_𝚇𝙳 <104634763+nobitaaaxd@users.noreply.github.com>
2024-05-03 23:41:22 +0530

    Update flirt.py

--- X/modules/user/flirt.py
+++ X/modules/user/flirt.py
@@ -34,11 +34,14 @@ from random import choice
 from pyrogram.types import Message
 from pyrogram import filters, Client
 from config import OWNER_ID
+from config import SUDO_USERS
 from config import CMD_HANDLER as cmd
 from XDB.data import FLIRT 
 from .help import *
 
-@Client.on_message(filters.command("flirt", cmd) & filters.me)
+@Client.on_message(
+    filters.command(["flirt"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
 async def flirt(x: Client, e: Message):
       NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
 
