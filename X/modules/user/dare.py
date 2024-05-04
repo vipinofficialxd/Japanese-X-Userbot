4eed75a971d29db082e2c86f1a811e24c480cc1f
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-05-04 09:58:07 +0530

    Update dare.py

--- X/modules/user/dare.py
+++ X/modules/user/dare.py
@@ -33,11 +33,14 @@ from random import choice
 from pyrogram.types import Message
 from pyrogram import filters, Client
 from config import OWNER_ID
+from config import SUDO_USERS
 from config import CMD_HANDLER as cmd
 from XDB.data import DARE
 from .help import *
 
-@Client.on_message(filters.command("dare", cmd) & filters.me)
+@Client.on_message(
+    filters.command(["dare"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
 async def dare(x: Client, e: Message):
       NOBI = "".join(e.text.split(maxsplit=1)[1:]).split(" ", 2)
 
