92b87f250384e781d7b97c3771f2cfc297f016ca
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-05-04 10:39:22 +0530

    Update anime1.py

--- X/modules/user/anime1.py
+++ X/modules/user/anime1.py
@@ -39,13 +39,16 @@ import requests
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import CMD_HANDLER as cmd
+from config import SUDO_USERS
 
 from .help import * 
 
 API_URL = "https://api.nekosapi.com/v3/images/random"
 
 
-@Client.on_message(filters.command("randomanime", cmd) & filters.me)
+@Client.on_message(
+    filters.command(["randomanime"], ".") & (filters.me | filters.user(SUDO_USERS))
+)
 async def random_anime(client: Client, message: Message):
     # Send the "Processing..." message
     await message.edit("Fetching a random anime image...")
