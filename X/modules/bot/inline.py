111ab43af0d8e61cd310df5e89593f80e7e5351b
Sʜɪᴢᴜᴋᴀ XD <168883994+shizukaaxd@users.noreply.github.com>
2024-05-05 10:45:11 +0530

    Update inline.py

--- X/modules/bot/inline.py
+++ X/modules/bot/inline.py
@@ -121,7 +121,7 @@ async def pingme(client: Client, message: Message):
 async def peler_function(message: Message, answers):
     msg = (
         f"Japanese-X-Userbot \n"
-        "ㅤㅤStatus : Ubot Active \n"
+        "ㅤㅤStatus : Japanese-X-Userbot Active \n"
         f"ㅤㅤㅤㅤModules:</b> <code>{len(modules)} Modules</code> \n"
         f"ㅤㅤㅤㅤBot Version: {BOT_VER} \n"
         f"ㅤㅤㅤㅤBranch: {branch} \n\n"
